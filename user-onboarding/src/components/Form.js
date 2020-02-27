8ef989460cda356623365989c957d305ac39bc14
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-27 13:23:24 -0700

    added header

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+const Form = (props) => {
+    return (
+        <section>
+            <h2>Sign up</h2>
+            <p>anyone can sign up</p>
+        </section>
+    )
+}
+
+export default Form;
---
+++ user-onboarding/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="/images/genie.gif" alt="funny genie"/>
+                <h1>Billy's Book Club</h1>
+                <img src="/images/genie.gif" alt="funny genie"/>
+            </div>
+            <div>
+                <p>A web by me (billy madison) forr mrs applegate's 3rd grade class!!</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
