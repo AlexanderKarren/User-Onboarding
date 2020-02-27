820fd0204cd064d839d16f9e1f84e4ba22374533
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-27 14:17:40 -0700

    updated form functionality, working required fields

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import './Form.css';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const Form = (props) => {
+const SignupForm = ({ values, touched, errors }) => {
     return (
         <section>
             <h2>Sign up</h2>
-            <p>anyone can sign up</p>
+            <p>anyone can sign up (as long as you are in 3rd grade and not LAME!)</p>
+            <Form>
+                <label>
+                    Name:
+                    <Field id="name" name="name" type="text" placeholder="name" />
+                </label>
+                {touched.name && errors.name && (<div>{errors.name}</div>)}
+                <label>
+                    E-mail:
+                    <Field id="email" name="email" type="email" placeholder="email" />
+                </label>
+                {touched.email && errors.email && (<div>{errors.email}</div>)}
+                <label>
+                    password:
+                    <Field id="password" name="password" type="password" placeholder="password" />
+                </label>
+                {touched.password && errors.password && (<div>{errors.password}</div>)}
+                <label>
+                    Teacher:
+                    <Field as="select" name="teacher">
+                        <option value="" disabled>Choose class</option>
+                        <option value="applegate">Mrs. Applegate</option>
+                        <option value="campbell">Mr. Campbell</option>
+                        <option value="lekebum">Mrs. Lekebum</option>
+                    </Field>
+                </label>
+                <label>
+                    <Field id="agreement" name="agreement" type="checkbox" checked={values.agreement} />
+                    I am a third grader and I am NOT LAME.
+                </label>
+                <button type="submit">SIGN UP!</button>
+            </Form>
         </section>
     )
 }
 
-export default Form;
+const FormikSignupForm = withFormik({
+    mapPropsToValues(props) {
+        return {
+            name: props.name || "",
+            email: props.email || "",
+            password: props.password || "",
+            teacher: props.teacher || "",
+            agreement: props.agreement || false,
+        }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("name is required!").max(20, "your name is too long!"),
+        email: Yup.string().required("no email? are you poor?").email("that's not an email!"),
+        password: Yup.string().required("please put in your password").min(8, "too short!").max(20, "too long!"),
+        agreement: Yup.string().required,
+    }),
+    handleSubmit(values) {
+        console.log(values);
+    }
+})(SignupForm);
+
+export default FormikSignupForm;
